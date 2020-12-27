d33932a9cb400b4333b12586fc7b48bacecdf399
Adrian Mare <adrian.mare@smart123.ro>
2020-12-27 15:22:19 +0200

    almost final

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Samsung Old AC
 
 Adds support for Samsung AC's before 2017
+Use for AC's that used to work with "Smart Air Conditioner" App
--- drivers/samsung_old_ac/device.js
+++ drivers/samsung_old_ac/device.js
@@ -1,28 +1,84 @@
 'use strict';
 
 const Homey = require('homey');
-const API = require('./samsung-discovery');
-const AirCon = require('./samsung-airconditioner');
+const API = require('../../lib/samsung-discovery');
+const AirCon = require('../../lib/samsung-airconditioner');
 
-class SamsungOldACDevice extends Homey.Device {
-	
+class SamsungOldACDevice extends Homey.Device {	
 	onInit() {
-        this.registerCapabilityListener('onoff', async ( value ) => {
-			this.log('wallplug value ', value);
+        this.aircon = new AirCon({
+            "ip": this.getSetting('IP'),
+            "duid": this.getSetting('MAC')
         });
 
-        this.aircons = [];
+        if (!this.getStoreValue('token')) {
+            this.getToken();
+        } else {
+            this.login(this.getStoreValue('token'));
+        }
+    }
 
-        var aircon = new AirCon({"ip":"adyku.asuscomm.com", "duid":"F8042E3011B5"});
-        aircon.getToken( (err, token) => {
+    getToken() {
+        this.aircon.getToken( (err, token) => {
             if (!!err) return console.log('login error: ' + err.message);
-            console.log(token);
+            this.setStoreValue('token', token);
+            this.login(token);
         }).on('waiting', function() {
             console.log('please power on the device within the next 30 seconds');
         }).on('end', function() {
             console.log('aircon disconnected');
         }).on('err', function(err) {
             console.log('aircon error: ' + err.message);
+        });
+    }
+
+    login(token) {
+        this.registerCapabilities();
+        this.registerEventListeners();
+
+        this.aircon.login(token, function(err) {
+            if (!!err) return console.log('login error: ' + err.message);
+        });
+    }
+
+    registerEventListeners() {
+        this.aircon.on('loginSuccess', () => {
+            this.aircon.getTemperature();
+        });
+
+        this.aircon.on('stateChange', (states) => {
+            for (var state in states) {
+                if (state == 'AC_FUN_TEMPNOW') {
+                    this.setCapabilityValue('measure_temperature', parseInt(states[state]));
+                }
+            }
+        });
+    }
+
+    registerCapabilities() {
+        this.registerCapabilityListener('onoff', async ( value ) => {
+            this.log('Turned: ', value);
+            this.aircon.onoff(value);
+
+            return Promise.resolve();
+        });
+
+        this.registerCapabilityListener('target_temperature', value => {
+            this.log('Set target temperature: ', value);
+            this.aircon.setTemperature(value);
+
+            return Promise.resolve();
+        });
+
+        this.registerCapabilityListener('thermostat_mode', value => {
+            this.log("Set mode: ", value);
+            if (value == "off") {
+                this.aircon.off();
+            } else {
+                this.aircon.mode(value);
+            }
+
+            return Promise.resolve();
         });
     }
 }
